d45abd0d8d38ae36b166f8f1374bad56e9f57421
Даниил <@WIN-ENF4NR8ET31>
2022-03-04 19:18:46 +0300

    With classes

--- TestModule/KrestikOneTwo.py
+++ TestModule/KrestikOneTwo.py
@@ -3,21 +3,25 @@ print ("Возврат хода - буква R")
 setka=int(input('введи размер желаемого поля: '))
 Choice=int(input('введи 1 для подключения одномерного массива|2 для двумерного: '))
 if Choice is 1:
-    OneArr.initArr(setka)
-    OneArr.ToConsoleArray(setka)
+    tarr=OneArr.ClassOneArr()
+    tarr.initArr(setka)
+    tarr.ToConsoleArray(setka)
     while True:
-        OneArr.vvod_XO(OneArr.GetSymbol(),setka)
-        OneArr.ToConsoleArray(setka)    
-        if OneArr.win(setka):
+        tarr.vvod_XO(tarr.GetSymbol(),setka)
+        tarr.ToConsoleArray(setka)    
+        if tarr.win(setka):
            print('Win!')
            break
 if Choice is 2:
-    TwoArr.initArr(setka)
-    TwoArr.ToConsoleArray(setka)
+    tarr=TwoArr.ClassTwoArr()
+    
+    tarr.initArr(setka)
+    tarr.ToConsoleArray(setka)
+
     while True:
 
-        TwoArr.vvod_XO(TwoArr.GetSymbol(),setka)
-        TwoArr.ToConsoleArray(setka)    
-        if TwoArr.win(setka):
+        tarr.vvod_XO(tarr.GetSymbol(),setka)
+        tarr.ToConsoleArray(setka)    
+        if tarr.win(setka):
            print('Win!')
            break
--- TestModule/OneArr.py
+++ TestModule/OneArr.py
@@ -1,102 +1,105 @@
-arraysetka=[]
-arraysteps=[]
-words = ("X", "O")
-def is_integer(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False 
-     
-def vvod_XO(bukva_,n):
-    x=input('следующий ход игрока' + ' '+ bukva_ +', выберите номер: ')
-
-    if x.upper()=='R' :
-        if  len(arraysteps)!=0:
-            stepp=arraysteps[len(arraysteps)-1]# последний ход
-            arraysetka[stepp-1]=stepp
-            arraysteps.pop(len(arraysteps)-1)
-    if is_integer(x) :
-        x=int(x)
+class ClassOneArr:
+    arraysetka=[]
+    arraysteps=[]
+    words = ("X", "O")
+    def __init__(self): 
+           pass
+    def is_integer(self,s):
         try:
-            if not(x<=n**2 and x>0 ):
-                raise BaseException("введи допустимое значение")
-            if not(x in arraysetka):
-                raise BaseException("поле занято")
-            arraysetka[x-1]=bukva_
-            arraysteps.append(x)
-        except BaseException as ve:
-          print(ve)
-          vvod_XO(bukva_,n)    
+            int(s)
+            return True
+        except ValueError:
+            return False 
+     
+    def vvod_XO(self,bukva_,n):
+        x=input('следующий ход игрока' + ' '+ bukva_ +', выберите номер: ')
+
+        if x.upper()=='R' :
+            if  len(self.arraysteps)!=0:
+                stepp=self.arraysteps[len(self.arraysteps)-1]# последний ход
+                self.arraysetka[stepp-1]=stepp
+                self.arraysteps.pop(len(self.arraysteps)-1)
+        if self.is_integer(x) :
+            x=int(x)
+            try:
+                if not(x<=n**2 and x>0 ):
+                    raise BaseException("введи допустимое значение")
+                if not(x in self.arraysetka):
+                    raise BaseException("поле занято")
+                self.arraysetka[x-1]=bukva_
+                self.arraysteps.append(x)
+            except BaseException as ve:
+              print(ve)
+              self.vvod_XO(bukva_,n)    
         
 
-def ResearchOfSize(current_element,maxlensymbol_):    
-    dlina=len(str(current_element))
-    return maxlensymbol_-dlina
+    def ResearchOfSize(self,current_element,maxlensymbol_):    
+        dlina=len(str(current_element))
+        return maxlensymbol_-dlina
 
 
-def vuvod_stroka(setka_,k): 
-    stroka='|'
-    maxlensymbol=len(str(setka_**2))
-    for i in range(k,k+setka_):
-        stroka+=str(arraysetka[i])
-        stroka+=' '*ResearchOfSize(arraysetka[i],maxlensymbol)
-        stroka+='|'
-    return stroka
+    def vuvod_stroka(self,setka_,k): 
+        stroka='|'
+        maxlensymbol=len(str(setka_**2))
+        for i in range(k,k+setka_):
+            stroka+=str(self.arraysetka[i])
+            stroka+=' '*self.ResearchOfSize(self.arraysetka[i],maxlensymbol)
+            stroka+='|'
+        return stroka
 
-def ToConsoleArray(setka_):    
-    t=0
-    for i in range(setka_):
-        stroka=vuvod_stroka(setka_,t)
-        print(stroka)
-        t+=setka_
+    def ToConsoleArray(self,setka_):    
+        t=0
+        for i in range(setka_):
+            stroka=self.vuvod_stroka(setka_,t)
+            print(stroka)
+            t+=setka_
 
-def askwin(win_combinations):   
-    for i in range(len(win_combinations)-1):
-        if win_combinations[i]!=win_combinations[i+1]:
-            return False
-    return True
+    def askwin(self,win_combinations):   
+        for i in range(len(win_combinations)-1):
+            if win_combinations[i]!=win_combinations[i+1]:
+                return False
+        return True
 
-def win(n):
-    k=0
-    mass=[]
-    # проверка по строкам
-    for i in range(n):
-        for s in range(k,k+n):
-            mass.append(arraysetka[s])
-        if askwin(mass):
-            return True
-        else:
-            k+=n
+    def win(self,n):
+        k=0
+        mass=[]
+        # проверка по строкам
+        for i in range(n):
+            for s in range(k,k+n):
+                mass.append(self.arraysetka[s])
+            if self.askwin(mass):
+                return True
+            else:
+                k+=n
 
-    mass.clear()
-        # проверка по столбцам
-    for i in range(n):  
         mass.clear()
-        for s in range(i,i+n*(n-1)+1,n):
-            mass.append(arraysetka[s])
-        if askwin(mass):
-            return True
+            # проверка по столбцам
+        for i in range(n):  
+            mass.clear()
+            for s in range(i,i+n*(n-1)+1,n):
+                mass.append(self.arraysetka[s])
+            if self.askwin(mass):
+                return True
 
-    mass.clear()
-     # проверка по диагоналям
-    for i in range(0,n**2,n+1):
-        mass.append(arraysetka[i])
-    if askwin(mass):
-            return True
+        mass.clear()
+         # проверка по диагоналям
+        for i in range(0,n**2,n+1):
+            mass.append(self.arraysetka[i])
+        if self.askwin(mass):
+                return True
 
-    mass.clear()
-    for i in range(n-1,n**2-n+1,n-1):
-        mass.append(arraysetka[i])
-    if askwin(mass):
-            return True
+        mass.clear()
+        for i in range(n-1,n**2-n+1,n-1):
+            mass.append(self.arraysetka[i])
+        if self.askwin(mass):
+                return True
         
-    return False
+        return False
 
-def GetSymbol():    
-   return words[len(arraysteps)%2] 
+    def GetSymbol(self):    
+       return self.words[len(self.arraysteps)%2] 
     
 
-def initArr(setka):
-   for i in range(1,setka**2+1):
-    arraysetka.append(i)
+    def initArr(self,setka):
+       for i in range(1,setka**2+1):
+        self.arraysetka.append(i)
