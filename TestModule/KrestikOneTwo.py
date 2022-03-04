1e113753296754fff051825430ab4beddafee4e8
Даниил <@WIN-ENF4NR8ET31>
2022-03-04 19:34:24 +0300

    OneArr  TwoArr

--- TestModule/KrestikOneTwo.py
+++ TestModule/KrestikOneTwo.py
@@ -2,26 +2,19 @@ import OneArr,TwoArr
 print ("Возврат хода - буква R")
 setka=int(input('введи размер желаемого поля: '))
 Choice=int(input('введи 1 для подключения одномерного массива|2 для двумерного: '))
+
 if Choice is 1:
     tarr=OneArr.ClassOneArr()
-    tarr.initArr(setka)
-    tarr.ToConsoleArray(setka)
-    while True:
-        tarr.vvod_XO(tarr.GetSymbol(),setka)
-        tarr.ToConsoleArray(setka)    
-        if tarr.win(setka):
-           print('Win!')
-           break
 if Choice is 2:
     tarr=TwoArr.ClassTwoArr()
     
-    tarr.initArr(setka)
-    tarr.ToConsoleArray(setka)
+tarr.initArr(setka)
+tarr.ToConsoleArray(setka)
 
-    while True:
+while True:
 
-        tarr.vvod_XO(tarr.GetSymbol(),setka)
-        tarr.ToConsoleArray(setka)    
-        if tarr.win(setka):
-           print('Win!')
-           break
+    tarr.vvod_XO(tarr.GetSymbol(),setka)
+    tarr.ToConsoleArray(setka)    
+    if tarr.win(setka):
+        print('Win!')
+        break
